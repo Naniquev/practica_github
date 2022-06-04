72ffd0cdc8637fea45783709b4aa252e9281b7a5
Natasha Quevedo <nanyque02@gmail.com>
2022-06-04 18:25:26 -0300

    Segundo commit

--- src/practicogit/PracticoGit.java
+++ src/practicogit/PracticoGit.java
@@ -1,13 +1,17 @@
 
 package practicogit;
 
+import java.util.Scanner;
+
 
 public class PracticoGit {
 
    
     public static void main(String[] args) {
         
+        Scanner leer = new Scanner(System.in);
         System.out.println("Ingrese el nombre se su mascota");
+        String nombre = leer.nextLine();
         
     }
     
